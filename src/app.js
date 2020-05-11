40894ea67495b09fc6a5d4d355a5409af0ecd65e
francisco17352 <57689459+francisco17352@users.noreply.github.com>
2020-05-11 17:30:24 +0100

    huhuehuehehu

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+//Configurações
+app.set('port', process.env.PORT || 3000);
+//Middlewares
+app.use(express.json());
+//Rotas
+app.use('/teste',(req,res)=>{
+res.send("Rota TESTE.");
+});
+app.use('/',(req,res)=>{
+res.send("Hello World");
+});
+app.listen(app.get('port'),()=>{
+console.log("Start server on port "+app.get('port'))
+})
