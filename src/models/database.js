7f83fc98d50c9c6652514a35ec99e889c217b923
Bernardo Mendonça <bernardomendonca1998@gmail.com>
2020-07-13 15:03:10 +0100

    teste

--- src/models/database.js
+++ src/models/database.js
@@ -1,10 +1,10 @@
 var Sequelize = require('sequelize');
 const sequelize = new Sequelize(
-'df43edtv6v0deo',  
-'ooghnsmfqxycne',
-'518db8eff26db3f5d83eacd239329689ce6d93a41dd74aea8694596c329a4f33',
+'d1ub2l1id4jhbn',  
+'etqpiywanzetqb',
+'611b8dd8cb41875b55d4ab110f93dc0424fa22eb68a23c2fbc8a9e0abce26035',
 {
-host: 'ec2-50-19-26-235.compute-1.amazonaws.com',
+host: 'ec2-176-34-123-50.eu-west-1.compute.amazonaws.com',
 port: '5432',
 dialect: 'postgres'
 }
