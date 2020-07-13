3a72033095adb5958053ffd6058022cfe9b51c99
Bernardo Mendonça <bernardomendonca1998@gmail.com>
2020-07-13 12:29:13 +0100

    backend

--- src/models/database.js
+++ src/models/database.js
@@ -1,10 +1,10 @@
 var Sequelize = require('sequelize');
 const sequelize = new Sequelize(
-'postgres',  
-'postgres',
-'bernardo98',
+'df43edtv6v0deo',  
+'ooghnsmfqxycne',
+'518db8eff26db3f5d83eacd239329689ce6d93a41dd74aea8694596c329a4f33',
 {
-host: 'localhost',
+host: 'ec2-50-19-26-235.compute-1.amazonaws.com',
 port: '5432',
 dialect: 'postgres'
 }
